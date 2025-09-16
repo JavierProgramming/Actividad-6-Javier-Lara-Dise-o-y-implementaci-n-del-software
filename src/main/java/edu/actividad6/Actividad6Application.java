6b8c15e18f9ae26155c04c370fbc4205ed6fe12e
Javier Eduardo Lara Garcia <158225079+JavierProgramming@users.noreply.github.com>
2025-09-16 12:26:34 -0600

    Actividad6Application.java

---
+++ src/main/java/edu/actividad6/Actividad6Application.java
@@ -0,0 +1,11 @@
+package edu.actividad6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad6Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Actividad6Application.class, args);
+    }
+}
